88189696836d80903af8af02698591e9429f90d5
evgeny.botylev <evgeny.botylev@x5.ru>
2025-03-01 12:56:24 +0300

    homework-10 review fixes

--- src/components/LanguageProvider/LanguageProvider.tsx
+++ src/components/LanguageProvider/LanguageProvider.tsx
@@ -1,25 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import '../../i18n/config';
-import { useTranslation, withTranslation, Trans } from 'react-i18next';
-//import { Context, Lang, Theme } from './Context';
-
-// function App() {
-//   const { t, i18n } = useTranslation();
-
-//   const count = 3;
-
-// //  type Lang = 'ru' | 'en';
-//   const [lang, setLang] = useState('ru');
-
-//   const toggleLang = () => {
-//     setLang((prevLang) => (prevLang === 'ru' ? 'en' : 'ru'));
-//     if (lang === 'ru') {
-//       i18n.changeLanguage('en');
-//     } else {
-//       i18n.changeLanguage('ru');
-//     }
-//   };
-
+import { useTranslation } from 'react-i18next';
 
 export type Language = 'ru' | 'en';
 type LanguageContext = { language: Language; toggleLanguage: () => void };
--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import './app/index.css';
 import App from './app/App';
 import './i18n/config';
 
-//import './i18n';
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
